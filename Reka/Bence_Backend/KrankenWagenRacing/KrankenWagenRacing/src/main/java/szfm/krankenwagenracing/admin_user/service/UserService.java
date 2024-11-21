f0ffc8e2e0650a8a03c7e16b643ccab36e96b5d5
reka03 <tamasireka03@gmail.com>
2024-11-21 16:21:06 +0100

    html fileok

---
+++ Reka/Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/admin_user/service/UserService.java
@@ -0,0 +1,9 @@
+package szfm.krankenwagenracing.admin_user.service;
+
+import szfm.krankenwagenracing.admin_user.dto.UserDto;
+import szfm.krankenwagenracing.admin_user.model.User;
+
+public interface UserService
+{
+    User save(UserDto userDto);
+}
---
+++ Reka/Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/src/test/java/szfm/krankenwagenracing/KrankenWagenRacingApplicationTests.java
@@ -0,0 +1,13 @@
+package szfm.krankenwagenracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrankenWagenRacingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
