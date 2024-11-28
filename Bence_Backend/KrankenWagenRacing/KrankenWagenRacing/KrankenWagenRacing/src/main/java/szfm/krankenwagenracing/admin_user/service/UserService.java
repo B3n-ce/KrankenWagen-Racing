fad9350ab272e7163fa7472fb18298b45c070de8
Ben-ce <szabobence864@gmail.com>
2024-11-28 15:55:40 +0100

    Bence_Backend

--- Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/admin_user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package szfm.krankenwagenracing.admin_user.service;
-
-import szfm.krankenwagenracing.admin_user.dto.UserDto;
-import szfm.krankenwagenracing.admin_user.model.User;
-
-public interface UserService
-{
-    User save(UserDto userDto);
-}
