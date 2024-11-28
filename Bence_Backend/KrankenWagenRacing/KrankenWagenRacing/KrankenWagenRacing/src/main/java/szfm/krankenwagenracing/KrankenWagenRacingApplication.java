be93ee9413b93855f63c288a387cdcb489e67084
Ben-ce <szabobence864@gmail.com>
2024-11-28 10:42:28 +0100

    Bence_Backend

---
+++ Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/KrankenWagenRacingApplication.java
@@ -0,0 +1,13 @@
+package szfm.krankenwagenracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrankenWagenRacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrankenWagenRacingApplication.class, args);
+    }
+
+}
