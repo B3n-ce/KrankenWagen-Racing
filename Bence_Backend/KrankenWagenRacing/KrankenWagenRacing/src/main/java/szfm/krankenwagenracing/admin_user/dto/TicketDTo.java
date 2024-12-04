9a038f625df1f4032383bddb953b68d5b70333a8
B3n-ce <szabobence864@gmail.com>
2024-12-04 14:51:53 +0100

    javitott_verzio

---
+++ Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/admin_user/dto/TicketDTo.java
@@ -0,0 +1,60 @@
+package szfm.krankenwagenracing.admin_user.dto;
+
+import java.util.Date;
+
+public class TicketDTo {
+    private String name;
+    private Integer price;
+    private String email;
+    private String type;
+    private String date;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public TicketDTo(String name, Integer price, String email, String date, String type) {
+        this.name = name;
+        this.price = price;
+        this.email = email;
+        this.date = date;
+        this.type = type;
+    }
+}
---
+++ Bence_Backend/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/admin_user/service/TicketServiceInterface.java
@@ -0,0 +1,13 @@
+package szfm.krankenwagenracing.admin_user.service;
+
+import szfm.krankenwagenracing.admin_user.dto.TicketDTo;
+import szfm.krankenwagenracing.admin_user.model.Ticket;
+
+import java.util.List;
+
+public interface TicketServiceInterface {
+    Ticket save(TicketDTo ticketDTo);
+
+    // Add this method to the interface
+    List<Ticket> findTicketsByEmail(String email);
+}
