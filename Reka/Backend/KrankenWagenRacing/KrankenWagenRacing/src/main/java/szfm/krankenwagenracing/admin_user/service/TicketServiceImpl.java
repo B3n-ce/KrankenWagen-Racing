8a714eb10ad2d3e6d9fa4190328b3d14fcf1f110
reka03 <tamasireka03@gmail.com>
2024-12-07 18:59:39 +0100

    project

---
+++ Reka/Backend/KrankenWagenRacing/KrankenWagenRacing/src/main/java/szfm/krankenwagenracing/admin_user/service/TicketServiceImpl.java
@@ -0,0 +1,32 @@
+package szfm.krankenwagenracing.admin_user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import szfm.krankenwagenracing.admin_user.model.Ticket;
+import szfm.krankenwagenracing.admin_user.repository.TicketRepository;
+import szfm.krankenwagenracing.admin_user.dto.TicketDTo;
+
+import java.util.List;
+
+@Service
+public class TicketServiceImpl implements TicketServiceInterface {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    public List<Ticket> findTicketsByEmail(String email) {
+        return ticketRepository.findByEmail(email);
+    }
+
+    @Override
+    public Ticket save(TicketDTo ticketDTo) {
+        Ticket ticket = new Ticket(ticketDTo.getName(), ticketDTo.getPrice(), ticketDTo.getEmail(), ticketDTo.getDate(), ticketDTo.getType());
+        return ticketRepository.save(ticket);
+    }
+
+    @Override
+    public void deleteTicketById(Long ticketId) {
+        ticketRepository.deleteById(ticketId); // Törlés a repository-ból
+    }
+}
